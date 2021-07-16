f9c86571426601802d85eee78bfa651dc623efd8
Nicolas Pettinato <npetti87@gmail.com>
2021-07-16 15:52:24 -0300

    implementación create cliente en service

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Cliente } from './cliente';
 import { CLIENTES } from "./clientes.json";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +10,16 @@ import { HttpClient } from "@angular/common/http";
 export class ClienteService {
 
   private urlEndpoint:string = "http://localhost:8080/api/clientes";
+  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
 
   constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]>{
       //return of(CLIENTES);
       return this.http.get<Cliente[]>(this.urlEndpoint);
+  }
+
+  create(cliente: Cliente) : Observable<Cliente>{
+     return this.http.post<Cliente>(this.urlEndpoint, cliente, {headers: this.httpHeaders});
   }
 }
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
 
 @Component({
   selector: 'app-form',
@@ -10,13 +12,14 @@ export class FormComponent implements OnInit {
   public cliente: Cliente = new Cliente();
   public titulo:string = "Nuevo Cliente";
   
-  constructor() { }
+  constructor(private clienteService: ClienteService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   public create(): void{ 
-     console.log("clicked");
-     console.log(this.cliente);
+    this.clienteService.create(this.cliente).subscribe(
+      response => this.router.navigate(['/clientes'])
+    );
   }
 }
